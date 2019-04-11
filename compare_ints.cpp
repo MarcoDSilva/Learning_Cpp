d1937ca4588ec0057d82fb7d70509e845ed26822
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2019-04-11 12:00:24 +0100

    Add files via upload
    comparing between 3 ints and returns the biggest one

---
+++ compare_ints.cpp
@@ -0,0 +1,35 @@
+//11-04-19
+//made by ms
+//comparar
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y, z;
+
+	//ask for the numbers to compare
+	cout << "Please insert the first number: ";		cin >> x;
+	cout << "Please insert the second number: ";	cin >> y;
+	cout << "Please insert the third number: "; 	cin >> z;
+
+	//conditional that compares the numbers between then and returns the biggest or if they are the same value
+	if (x > y && x > z)
+	{
+		cout << x << " is the biggest number between " << x << " and " << y << " and " << z;
+
+	}
+	else if (y > z && y > x)
+	{
+		cout << y << " is the biggest number between " << x << " and " << y << " and " << z;
+	}
+	else if (z > x && z > y)
+	{
+		cout << z << " is the biggest number between " << x << " and " << y << " and " << z;
+	}
+	else
+	{
+		cout << "All 3 numbers have the same value." << endl;
+	}
+}
