17fd886b5bde10e283b8ae3a73a69ea4ede361dc
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2019-04-19 15:13:52 +0100

    Update IMC.cpp
    update with extra checks (if it's normal weight or not)

--- IMC.cpp
+++ IMC.cpp
@@ -1,5 +1,5 @@
-//12/04/2019
-// IMC calculation
+/* 19-04-2019
+IMC calculation*/
 
 #include <iostream>
 #include <math.h>
@@ -10,12 +10,28 @@ int main()
 	float weight, height, IMC;
 
 	//asks user for height and weight
-	cout << "Please insert your weight: ";		cin >> weight;
-	cout << "Please insert your height: ";		cin >> height;
+	cout << "Please insert your weight: ";	cin >> weight;
+	cout << "Please insert your height: ";	cin >> height;
 
 	//calculus
 	IMC = (weight / (height * height));
 
-	cout << "Your IMC is: " << IMC << endl;
+	//checks the value and if the user is between the expected values
+	if (IMC <= 18.5)
+	{
+		cout << "You are underweight and getting a bit more kilos would help. Your IMC value is: " << IMC;
+	}
+	else if (IMC > 18.5 && IMC <= 25)
+	{
+		cout << "You have a normal weight. Good job! Your IMC value is: " << IMC;
+	}
+	else if (IMC > 25 && IMC <= 30)
+	{
+		cout << "You are above your normal weight. Try to lose some kilos/grams. Your IMC is: " << IMC;
+	}
+	else
+	{
+		cout << "You are obese! Go check the doctor right away!! Your IMC is: " << IMC;
+	}
 
 }
